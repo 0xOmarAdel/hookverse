6f01fd7b705a4c0f3acb3c89d7c2c6cfe4992026
Heisenberg <omaradelawad20013@gmail.com>
2024-02-09 05:32:24 +0200

    created useFetch hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,42 @@
+import { useCallback, useState } from "react";
+import { RequestConfig } from "../types";
+
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const sendRequest = useCallback(
+    async (requestConfig: RequestConfig, applyData: (data: any) => void) => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method || "GET",
+          headers: requestConfig.headers || {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
+
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+
+        const data = await response.json();
+        applyData(data);
+      } catch (err: any) {
+        setError(err.message || "Something went wrong!");
+      }
+
+      setIsLoading(false);
+    },
+    []
+  );
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useFetch;
