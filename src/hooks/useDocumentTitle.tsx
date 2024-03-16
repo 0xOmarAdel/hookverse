ab8275446c968968867eaed9be21dd8a6437e084
Heisenberg <omaradelawad20013@gmail.com>
2024-03-16 17:46:07 +0200

    feat: Add cleanup function to useDocumentTitle hook

--- src/hooks/useDocumentTitle.tsx
+++ src/hooks/useDocumentTitle.tsx
@@ -2,7 +2,14 @@ import { useEffect } from "react";
 
 const useDocumentTitle = (title: string) => {
   useEffect(() => {
+    if (!title) return;
+    const oldTitle = document.title;
+
     document.title = title;
+
+    return () => {
+      document.title = oldTitle;
+    };
   }, [title]);
 };
 
